4ca017bee3831b3b42eaf1e25ba4c7459e68d775
AK6SM <akshayasm6@gmail.com>
2024-04-26 17:23:10 +0530

    logo

--- client/src/components/common/header/Header.jsx
+++ client/src/components/common/header/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
       <header>
         <div className='container flex'>
           <div className='logo'>
-            <img src='../../../../public/images/logo.PNG' alt='' />
+            <img src='./images/logo.PNG' alt='' />
           </div>
           <div className='nav'>
             <ul className={navList ? "large" : "flex"}>
