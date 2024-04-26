f8fcf6bac8afc45d0106af591d38d87376a15a00
AK6SM <akshayasm6@gmail.com>
2024-04-26 17:31:38 +0530

    form data

--- server/database.js
+++ server/database.js
@@ -1,9 +1,9 @@
 const {Pool} = require("pg");
 
 const pool = new Pool({
-    user: "postgres",
-    password:"root",
-    host:"localhost",
+    user: "dhrona_user",
+    password:"ICH8RQ2JUQK30iVgkzvy2tpx4QWmM6jW",
+    host:"colo2sgl5elc73blmk90-a.oregon-postgres.render.com",
     port:5432,
     database:"dhrona"
 });
