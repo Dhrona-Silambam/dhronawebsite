494d368f17a191db31b66a5e622880726c77c250
AK6SM <akshayasm6@gmail.com>
2024-04-26 17:50:25 +0530

    form data

--- server/database.js
+++ server/database.js
@@ -1,11 +1,12 @@
-const {Pool} = require("pg").native;
+const {Pool} = require("pg");
 
 const pool = new Pool({
     user: "dhrona_user",
     password:"ICH8RQ2JUQK30iVgkzvy2tpx4QWmM6jW",
     host:"colo2sgl5elc73blmk90-a.oregon-postgres.render.com",
     port:5432,
-    database:"dhrona"
+    database:"dhrona",
+    ssl: {rejectUnauthorized: false}
 });
 
 
